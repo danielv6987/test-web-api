d12ec8aeb0125590e38f931bfbe3b2d3cd486aff
Daniel Vecchi <dvecchi@andplus.com>
2021-09-22 13:24:23 -0400

    adds new controller

--- Controllers/VecdController.cs
+++ Controllers/VecdController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
@@ -8,16 +8,17 @@ namespace TestWebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class VecdController : ControllerBase
     {
+        // GET
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracings", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<VecdController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public VecdController(ILogger<VecdController> logger)
         {
             _logger = logger;
         }
