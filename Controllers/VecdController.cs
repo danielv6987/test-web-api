b54b73884b1ad5048ce82e5dab03d06681a71d68
Daniel <dvecchi@andplus.com>
2021-09-22 14:21:19 -0400

    test

--- Controllers/VecdController.cs
+++ Controllers/VecdController.cs
@@ -26,6 +26,530 @@ namespace TestWebApi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            int i = 0;
+            while (true)
+            {
+                if (i == 10)
+                {
+                    break;      // Non-Compliant
+                }
+
+                Console.WriteLine(i);
+                i++;
+            }
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
