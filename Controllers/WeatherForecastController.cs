d7cb3670543c8f6383b543ded3cbf9da12321606
Daniel Vecchi <dvecchi@andplus.com>
2021-09-22 20:22:39 -0400

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -38,12 +38,11 @@ namespace TestWebApi.Controllers
                 i++;
             }
 
-            var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
                     Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
+                    TemperatureC = 20,
+                    Summary = "test"
                 })
                 .ToArray();
         }
